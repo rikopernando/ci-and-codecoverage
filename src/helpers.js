a87d0e85b20c4629077a1530e533a0eecaaf9173
Riko Pernando <rikopernando96@gmail.com>
2020-04-13 20:30:52 +0700

    test master (#4)

--- src/helpers.js
+++ src/helpers.js
@@ -1,13 +1,10 @@
-// incrementQty
 const incrementQty = (qty) => parseInt(qty) + 1;
 
-// decrementQty
 const decrementQty = (qty) => {
   if (qty > 1) return qty - 1
   return 1
 }
 
-// recalculateSubtotal
 const recalculateSubtotal = (price, qty, discount = 0) => {
   return (1 - parseFloat(discount) / 100) * price * qty
 }
